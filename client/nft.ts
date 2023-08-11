1d6224401fc998ac33cc010ec065160da9fa6df2
khatriamit <khatriamit054@gmail.com>
2023-08-11 14:56:21 +0545

    fix contract issue

--- client/nft.ts
+++ client/nft.ts
@@ -1,40 +1,21 @@
-import { TransactionBlock } from "@mysten/sui.js/transactions"
-import { RawSigner } from "@mysten/sui.js/src/signers/raw-signer";
+import dotenv from "dotenv";
+import { TransactionBlock } from "@mysten/sui.js"
+import { RawSigner } from "@mysten/sui.js";
 import signerContext from "./utils/signer";
+dotenv.config()
 
-
-
-const mintTs = async () => {
-  const { address, signer, provider } = signerContext();
-  const tx = new TransactionBlock();
-
-  let color = tx.pure("Red", "string");
-  let weight = tx.pure("50", "u32");
-  const res = tx.moveCall({
-    target: '0xfa54ab370242c021c23752470f3396116d3858cb2c1c393f617678d55af35b1e::nft::mint',
-    arguments: [color, weight],
-    typeArguments: [],
-  });
-
-  const result = signer.signAndExecuteTransactionBlock({
-    transactionBlock: tx,
-    requestType: "WaitForLocalExecution",
-    options: {
-      showEffects: true
-    }
-  });
-  console.log(result);
-}
+const package_id = process.env.PACKAGE_ID  as string;
 
 const mint_with_exact_payment = async (address: string, signer: RawSigner) => {
   const exactPaymentInMIST = 100000;
   const tx = new TransactionBlock();
 
   tx.setSender(address);
-
+  console.log(package_id);
+  
   const fee = tx.splitCoins(tx.gas, [tx.pure(exactPaymentInMIST)]);
   const nft = tx.moveCall({
-    target: `0xfa54ab370242c021c23752470f3396116d3858cb2c1c393f617678d55af35b1e::nft::costly_mint`,
+    target: `${package_id}::nft::costly_mint`,
     arguments: [fee]
   });
   tx.transferObjects([nft], tx.pure(address));
@@ -59,9 +40,9 @@ const mint = async (
   const tx = new TransactionBlock();
 
   tx.setSender(address);
-
+  
   const nft = tx.moveCall({
-    target: `$0xfa54ab370242c021c23752470f3396116d3858cb2c1c393f617678d55af35b1e::nft::mint`,
+    target: `${package_id}::nft::mint`,
     arguments: [tx.pure(color, "string"), tx.pure(weight, "u32")],
     typeArguments: [],
   });
@@ -74,7 +55,7 @@ const mint = async (
     transactionBlock: tx,
     requestType: "WaitForLocalExecution",
     options: {
-      showEffects: true,
+      showEffects: false,
     },
   });
 
@@ -83,25 +64,10 @@ const mint = async (
 
 const main = async () => {
   const { address, signer, provider } = signerContext();
-  let result;
-  //call mint
   const color = "amit";
   const weight = 28;
 
-
-  result = await mint(color, weight, address, signer);
-
-  //   //call mint_and_change
-  //   //   result = await mint_and_change(color, weight, address, signer);
-
-  //   // mint with payment
-  //   // result = await mint_with_exact_payment(address, signer);
-
-  //   // call bad example
-  //   // result = await bad_call(color, weight, address, signer);
-
-  //   // call mass mint
-  //   // result = await mass_mint_correct(address, signer);
+  const result = await mint(color, weight, address, signer);
   console.log(JSON.stringify(result));
 };
 
