42c5d7f12cf15f901d9cf7ec96a40d213e4b4fd2
khatriamit <khatriamit054@gmail.com>
2023-08-12 20:53:00 +0545

    refactor profile

--- client/scripts/profile.ts
+++ client/scripts/profile.ts
@@ -1,21 +1,28 @@
 
-import { TransactionBlock } from "@mysten/sui.js"
-import signerContext from "./utils/signer";
+import dotenv from "dotenv";
+import { TransactionBlock } from "@mysten/sui.js/transactions"
+import signerContext from "../utils/signer";
 
-const createProfile = async () => {
-  const { address, signer, provider } = signerContext();
+dotenv.config();
+
+const packageId = process.env.PACKAGE_ID as string
+
+
+async function createProfile() {
+  const { address, keyPair, client } = signerContext();
   const tx = new TransactionBlock();
   let name = tx.pure("amit", "string");
   let url = tx.pure("amit.com/", "string");
-  const res = tx.moveCall({
-    target: `0x42e6b25e4fe4c97e6ddc2301d18736319316054adc02bc9409d13d8ff485890b::profile::create_profile`,
+  tx.moveCall({
+    target: `${packageId}::profile::create_profile`,
     arguments: [name, url],
     typeArguments: []
   });
 
-  const result = signer.signAndExecuteTransactionBlock({
+  const result = await client.signAndExecuteTransactionBlock({
     transactionBlock: tx,
     requestType: "WaitForLocalExecution",
+    signer:keyPair,
     options: {
       showEffects: true
     }
@@ -23,3 +30,4 @@ const createProfile = async () => {
   console.log(result);
 
 }
+createProfile()
